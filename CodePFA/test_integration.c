556e9d69f669545ef3b6a6009f5f716dd564f8c2
DE PALACIOS Borja <mwizdi@gmail.com>
2026-02-11 17:01:27 +0100

    a

--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -6,8 +6,23 @@
 #include "integration.h"
 
 /* Code here everything you need to test the integration methods and show your numericzal results */
+double f(double t)
+return sin(t);
 
 int main()
 {
-  return 0;
+  QuadFormula qf;
+  double I1,I2,I3,I4;
+  printf("Left : \n %d" , setQuadFormula(&qf, "left"));
+  I1 = integrate(f, 0, M_PI, 5, &qf);
+  printf("%f\n", I1);
+  printf("Left : \n %d" , setQuadFormula(&qf, "right"));
+  I2 = integrate(f, 0, M_PI, 5, &qf);
+  printf("%f\n", I2);
+  printf("Left : \n %d" , setQuadFormula(&qf, "middle"));
+  I3 = integrate(f, 0, M_PI, 5, &qf);
+  printf("%f\n", I3);
+  printf("Left : \n %d" , setQuadFormula(&qf, "trapezes"));
+  I4 = integrate(f, 0, M_PI, 5, &qf);
+  printf("%f\n", I4);
 }
