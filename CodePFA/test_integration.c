53a6f661aedb43ceb3eb7613871fb23947fd5713
DE PALACIOS Borja <mwizdi@gmail.com>
2026-02-18 12:30:48 +0100

    a

--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -6,23 +6,23 @@
 #include "integration.h"
 
 /* Code here everything you need to test the integration methods and show your numericzal results */
-double f(double t)
+double f(double t){
 return sin(t);
-
+}
 int main()
 {
   QuadFormula qf;
   double I1,I2,I3,I4;
   printf("Left : \n %d" , setQuadFormula(&qf, "left"));
-  I1 = integrate(f, 0, M_PI, 5, &qf);
+  I1 = integrate(f, 0, M_PI, 1, &qf);
   printf("%f\n", I1);
-  printf("Left : \n %d" , setQuadFormula(&qf, "right"));
-  I2 = integrate(f, 0, M_PI, 5, &qf);
+  printf("Right : \n %d" , setQuadFormula(&qf, "right"));
+  I2 = integrate(f, 0, M_PI, 1, &qf);
   printf("%f\n", I2);
-  printf("Left : \n %d" , setQuadFormula(&qf, "middle"));
-  I3 = integrate(f, 0, M_PI, 5, &qf);
+  printf("Middle : \n %d" , setQuadFormula(&qf, "middle"));
+  I3 = integrate(f, 0, M_PI, 1, &qf);
   printf("%f\n", I3);
-  printf("Left : \n %d" , setQuadFormula(&qf, "trapezes"));
-  I4 = integrate(f, 0, M_PI, 5, &qf);
+  printf("Trapezes : \n %d" , setQuadFormula(&qf, "trapezes"));
+  I4 = integrate(f, 0, M_PI, 1, &qf);
   printf("%f\n", I4);
 }
