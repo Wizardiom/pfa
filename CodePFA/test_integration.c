626320bfa18735c1b55de24f0bf80d630c1396a0
DE PALACIOS Borja <mwizdi@gmail.com>
2026-02-18 16:18:55 +0100

    a

--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -12,21 +12,33 @@ return sin(t);
 int main()
 {
   QuadFormula qf;
-  double I1,I2,I3,I4;
+  double I1,I2,I3,I4, I5,I6,I7;
   printf("Left : \n");
   setQuadFormula(&qf, "left");
-  I1 = integrate(f, 0, M_PI, 1, &qf);
+  I1 = integrate_dx(f, 0, M_PI, 0.1, &qf);
   printf("%f\n", I1);
   printf("Right : \n");
   setQuadFormula(&qf, "right");
-  I2 = integrate(f, 0, M_PI, 1, &qf);
+  I2 = integrate_dx(f, 0, M_PI, 0.1, &qf);
   printf("%f\n", I2);
   printf("Middle : \n");
   setQuadFormula(&qf, "middle");
-  I3 = integrate(f, 0, M_PI, 1, &qf);
+  I3 = integrate_dx(f, 0, M_PI, 0.1, &qf);
   printf("%f\n", I3);
   printf("Trapezes : \n");
   setQuadFormula(&qf, "trapezes");
-  I4 = integrate(f, 0, M_PI, 1, &qf);
+  I4 = integrate_dx(f, 0, M_PI, 0.1, &qf);
   printf("%f\n", I4);
+  printf("Simpson : \n");
+  setQuadFormula(&qf, "simpson");
+  I5 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  printf("%f\n", I5);
+  printf("Gauss2 : \n");
+  setQuadFormula(&qf, "gauss2");
+  I6 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  printf("%f\n", I6);
+  printf("Gauss3 : \n");
+  setQuadFormula(&qf, "gauss3");
+  I7 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  printf("%f\n", I7);
 }
