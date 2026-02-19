ca29908b0cb2af378d4a7bfb1fb10a54d29064d8
DE PALACIOS Borja <mwizdi@gmail.com>
2026-02-19 14:10:14 +0100

    a

--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -7,7 +7,7 @@
 
 /* Code here everything you need to test the integration methods and show your numericzal results */
 double f(double t){
-return sin(t);
+return log(t);
 }
 int main()
 {
@@ -15,30 +15,30 @@ int main()
   double I1,I2,I3,I4, I5,I6,I7;
   printf("Left : \n");
   setQuadFormula(&qf, "left");
-  I1 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I1 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I1);
   printf("Right : \n");
   setQuadFormula(&qf, "right");
-  I2 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I2 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I2);
   printf("Middle : \n");
   setQuadFormula(&qf, "middle");
-  I3 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I3 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I3);
   printf("Trapezes : \n");
   setQuadFormula(&qf, "trapezes");
-  I4 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I4 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I4);
   printf("Simpson : \n");
   setQuadFormula(&qf, "simpson");
-  I5 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I5 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I5);
   printf("Gauss2 : \n");
   setQuadFormula(&qf, "gauss2");
-  I6 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I6 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I6);
   printf("Gauss3 : \n");
   setQuadFormula(&qf, "gauss3");
-  I7 = integrate_dx(f, 0, M_PI, 0.1, &qf);
+  I7 = integrate(f, 1, 2, 10, &qf);
   printf("%f\n", I7);
 }
